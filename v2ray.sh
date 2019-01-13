9e9d982a1a31f9d38fd1bf3b1b2c2f7d88d00b94
233boy <onehack.ttt@gmail.com>
2019-01-13 15:51:46 +0800

    tg proxy secret add "dd" prefix

--- v2ray.sh
+++ v2ray.sh
@@ -10,7 +10,7 @@ none='\e[0m'
 # Root
 [[ $(id -u) != 0 ]] && echo -e " 哎呀……请使用 ${red}root ${none}用户运行 ${yellow}~(^_^) ${none}" && exit 1
 
-_version="v3.17-test"
+_version="v3.18-test"
 
 cmd="apt-get"
 
